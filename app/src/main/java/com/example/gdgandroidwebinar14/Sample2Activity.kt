e34f3aaf86e9719ad13bc451bbf054a85e77fff9
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2020-11-01 23:31:49 +0100

    Sample 2. Getting and setting motion progress programatically.

--- app/src/main/java/com/example/gdgandroidwebinar14/Sample2Activity.kt
+++ app/src/main/java/com/example/gdgandroidwebinar14/Sample2Activity.kt
@@ -1,11 +1,40 @@
 package com.example.gdgandroidwebinar14
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_sample2.*
 
 class Sample2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample2)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser)
+                    motionLayout2.progress = progress / 100f
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) = Unit
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) = Unit
+
+        })
+
+        motionLayout2.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) = Unit
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) {
+                seekBar.progress = (progress * 100).toInt()
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) = Unit
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) =
+                Unit
+
+        })
     }
 }
